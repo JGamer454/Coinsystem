ecf6ea0ee4d44223ac6cfe14c880cb33c202b806
JGamer454 <jakob.geisenhainer@icloud.com>
2021-07-04 09:02:47 +0200

    Main und plugin.yml erstellt

---
+++ src/main/java/de/jgamer454/coinsystem/main/Main.java
@@ -0,0 +1,11 @@
+package de.jgamer454.coinsystem.main;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+}
